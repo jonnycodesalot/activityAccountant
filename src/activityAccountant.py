7107f19a4a36dffd5bf6039300b0960a8ca9ea5c
Jonathan Hearn <jonnycodesalot@gmail.com>
2024-09-11 11:28:50 -0400

    python formatting

--- src/activityAccountant.py
+++ src/activityAccountant.py
@@ -7,14 +7,16 @@ EVENT_SUBDIR = "eventExports/"
 OUTPUT_SUBDIR = "scoring/"
 SCORE_FILE = "scoring.xlsx"
 
+
 class Event:
-    def __init__(self,name,date,activityPoints):
+    def __init__(self, name, date, activityPoints):
         self.name = name
         self.date = date
         self.activityPoints = activityPoints
-        
+
     def __str__(self):
         return f"{self.name} {self.date} - {self.activityPoints} points\n"
+
 
 class Attendee:
     def __init__(self, firstName, lastName, email):
@@ -23,20 +25,22 @@ class Attendee:
         self.email = email
         self.points = 0
         self.attended = list()
-    
+
     def __str__(self):
-        str = f"{self.firstName} {self.lastName} - {self.points} points - {self.email}\n"
+        str = (
+            f"{self.firstName} {self.lastName} - {self.points} points - {self.email}\n"
+        )
         for event in self.attended:
             str += f"\t{event}\n"
         return str
 
+
 class Accountant:
-    def __init__(self,inputDir, outputDir):
+    def __init__(self, inputDir, outputDir):
         self.userMap = dict()
         self.eventMap = dict()
         self.inputBaseDir = inputDir
         self.outputBaseDir = outputDir
-    
 
     def combineDuplicateUsers(self):
         # Placeholder. We're keying users on email address, but if we
@@ -46,16 +50,16 @@ class Accountant:
 
     def getUser(self, firstName, lastName, email):
         email = email.strip()
-        if (not self.userMap.__contains__(email)):
-            self.userMap[email] = Attendee(firstName.strip(),lastName.strip(),email)
+        if not self.userMap.__contains__(email):
+            self.userMap[email] = Attendee(firstName.strip(), lastName.strip(), email)
         return self.userMap[email]
-    
+
     def addUniqueEvent(self, name, date, pointCount):
         name = name.strip()
-        if (not self.eventMap.__contains__(name)):
-            self.eventMap[name] = Event(name,date.strip(),int(pointCount))
+        if not self.eventMap.__contains__(name):
+            self.eventMap[name] = Event(name, date.strip(), int(pointCount))
         return self.eventMap[name]
-    
+
     def printAttendees(self):
         for key in self.userMap:
             print(self.userMap[key])
@@ -70,42 +74,45 @@ class Accountant:
         spreadsheet = pd.ExcelFile(directory + file)
         sheetCount = spreadsheet.sheet_names.__len__()
         if sheetCount != 1:
-            raise Exception(f"File {file} has {sheetCount} sheets, but we only support single-sheet XLSX files.\n")
-        sheet = pd.read_excel(spreadsheet,spreadsheet.sheet_names[0])
+            raise Exception(
+                f"File {file} has {sheetCount} sheets, but we only support single-sheet XLSX files.\n"
+            )
+        sheet = pd.read_excel(spreadsheet, spreadsheet.sheet_names[0])
         return sheet
 
     def buildEventList(self):
         eventDir = self.inputBaseDir + EVENT_SUBDIR
         for file in os.listdir(eventDir):
             sheet = self.openAndValidateSheet(eventDir, file)
-            if (sheet is None):
+            if sheet is None:
                 continue
-            for ndx in range(0,sheet.__len__()):
-                activityPoints = sheet['activity_points'].iloc[ndx]
+            for ndx in range(0, sheet.__len__()):
+                activityPoints = sheet["activity_points"].iloc[ndx]
                 if (activityPoints == 0) or math.isnan(activityPoints):
                     # No point looking at events with no point count
                     continue
                 activityPoints = activityPoints
-                eventName = sheet['title'].iloc[ndx]
-                eventDate = sheet['event_date'].iloc[ndx]
-                self.addUniqueEvent(eventName,eventDate,activityPoints)
-                
+                eventName = sheet["title"].iloc[ndx]
+                eventDate = sheet["event_date"].iloc[ndx]
+                self.addUniqueEvent(eventName, eventDate, activityPoints)
+
     def buildAttendeeList(self):
         registrantDir = self.inputBaseDir + REGISTRANT_SUBDIR
         for file in os.listdir(registrantDir):
             sheet = self.openAndValidateSheet(registrantDir, file)
-            if (sheet is None):
+            if sheet is None:
                 continue
             print(f"Processing Registrant Export {file}...")
-            for ndx in range(0,sheet.__len__()):
-                if (sheet['Payment Status'].iloc[ndx] != 'Paid'):
+            for ndx in range(0, sheet.__len__()):
+                if sheet["Payment Status"].iloc[ndx] != "Paid":
                     # Skip records that are cancelled or pending
                     continue
                 attendee = self.getUser(
-                    firstName=sheet['First Name'].iloc[ndx],
-                    lastName=sheet['Last Name'].iloc[ndx],
-                    email=sheet['Email'].iloc[ndx])
-                attendee.attended.append(str(sheet['Event'].iloc[0]))
+                    firstName=sheet["First Name"].iloc[ndx],
+                    lastName=sheet["Last Name"].iloc[ndx],
+                    email=sheet["Email"].iloc[ndx],
+                )
+                attendee.attended.append(str(sheet["Event"].iloc[0]))
                 # diagnostic print
                 # print(f"\t Attended by {attendee.firstName} {attendee.lastName} - {attendee.email}: total is {attendee.points} points")
 
@@ -115,7 +122,7 @@ class Accountant:
             for attendeeEmail, attendee in self.userMap.items():
                 if eventName in attendee.attended:
                     attendee.points += event.activityPoints
-    
+
     def exportResults(self):
         scoringDir = self.outputBaseDir + OUTPUT_SUBDIR
         firstNames = list()
@@ -127,20 +134,23 @@ class Accountant:
             lastNames.append(attendee.lastName)
             emails.append(attendeeEmail)
             points.append(attendee.points)
-        dataFrame = pd.DataFrame({
-            'First Name':firstNames,
-            'Last Name':lastNames,
-            'Email': emails,
-            'ActivityPoints':points})
+        dataFrame = pd.DataFrame(
+            {
+                "First Name": firstNames,
+                "Last Name": lastNames,
+                "Email": emails,
+                "ActivityPoints": points,
+            }
+        )
         os.makedirs(os.path.dirname(scoringDir), exist_ok=True)
         dataFrame.to_excel(scoringDir + SCORE_FILE)
 
 
 if __name__ == "__main__":
-    accountant = Accountant("./test/","/tmp/")
+    accountant = Accountant("./test/", "/tmp/")
     accountant.buildEventList()
     accountant.buildAttendeeList()
-    #accountant.printEvents()
+    # accountant.printEvents()
     accountant.assignPoints()
-    #accountant.printAttendees()
+    # accountant.printAttendees()
     accountant.exportResults()
--- src/googleDriveClient.py
+++ src/googleDriveClient.py
@@ -11,39 +11,49 @@ from apiclient import http
 import logging
 import io
 
-CREDSFILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+CREDSFILE = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
 
 # To list folders
-def downloadDirectory(service, filid, des):
-    
-  #     var q = "mimeType = 'application/vnd.google-apps.folder' and '"+folderId+"' in parents";
-  # var children = Drive.Files.list({q:q});
-    q = "'"+filid+"' in parents"
-    results = service.files().list(
-        pageSize=1000, q=q,
-        fields="nextPageToken, files(id, name, mimeType)",
-              includeItemsFromAllDrives=True,
-              supportsAllDrives=True).execute()
+def downloadDirectory(service, fileId, des):
+
+    #     var q = "mimeType = 'application/vnd.google-apps.folder' and '"+folderId+"' in parents";
+    # var children = Drive.Files.list({q:q});
+    q = "'" + fileId + "' in parents"
+    results = (
+        service.files()
+        .list(
+            pageSize=1000,
+            q=q,
+            fields="nextPageToken, files(id, name, mimeType)",
+            includeItemsFromAllDrives=True,
+            supportsAllDrives=True,
+        )
+        .execute()
+    )
     logging.debug(results)
-    folder = results.get('files', [])
+    folder = results.get("files", [])
     logging.debug(folder)
     for item in folder:
-        if str(item['mimeType']) == str('application/vnd.google-apps.folder'):
-            if not os.path.isdir(des+"/"+item['name']):
-                os.mkdir(path=des+"/"+item['name'])
-            print(item['name'])
-            downloadDirectory(service, item['id'], des+"/"+item['name'])  # LOOP un-till the files are found
-        elif (item['mimeType'] == str('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')):
-            downloadSpreadsheet(service, item['id'], item['name'], des)
-            print(item['name'])
+        if str(item["mimeType"]) == str("application/vnd.google-apps.folder"):
+            if not os.path.isdir(des + "/" + item["name"]):
+                os.mkdir(path=des + "/" + item["name"])
+            print(item["name"])
+            downloadDirectory(
+                service, item["id"], des + "/" + item["name"]
+            )  # LOOP un-till the files are found
+        elif item["mimeType"] == str(
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        ):
+            downloadSpreadsheet(service, item["id"], item["name"], des)
+            print(item["name"])
         else:
-           print(f"Skipping download of non-spreadsheet file {item['name']}")
+            print(f"Skipping download of non-spreadsheet file {item['name']}")
     return folder
 
 
 # To Download Files
-def downloadSpreadsheet(service, dowid, name,dfilespath):
+def downloadSpreadsheet(service, dowid, name, dfilespath):
     request = service.files().get_media(fileId=dowid)
     fh = io.BytesIO()
     downloader = http.MediaIoBaseDownload(fh, request)
@@ -51,90 +61,120 @@ def downloadSpreadsheet(service, dowid, name,dfilespath):
     while done is False:
         status, done = downloader.next_chunk()
         print("Download %d%%." % int(status.progress() * 100))
-    with io.open(dfilespath + "/" + name, 'wb') as f:
+    with io.open(dfilespath + "/" + name, "wb") as f:
         fh.seek(0)
         f.write(fh.read())
+
 
 # def uploadSpreadsheet(service,)
 
 
 def main():
-  """Shows basic usage of the Drive v3 API.
-  Prints the names and ids of the first 10 files the user has access to.
-  """
-  print(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
+    #   creds = None
+    #   # The file token.json stores the user's access and refresh tokens, and is
+    #   # created automatically when the authorization flow completes for the first
+    #   # time.
+    #   if os.path.exists("token.json"):
+    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    #   # If there are no (valid) credentials available, let the user log in.
+    #   if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #       creds.refresh(Request())
+    #     else:
+    #       flow = InstalledAppFlow.from_service_account_file(
+    #           "/tmp/activityAccountant/creds/credentials.json", SCOPES
+    #       )
+    #       creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open("token.json", "w") as token:
+    #       token.write(creds.to_json())
 
-#   creds = None
-#   # The file token.json stores the user's access and refresh tokens, and is
-#   # created automatically when the authorization flow completes for the first
-#   # time.
-#   if os.path.exists("token.json"):
-#     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-#   # If there are no (valid) credentials available, let the user log in.
-#   if not creds or not creds.valid:
-#     if creds and creds.expired and creds.refresh_token:
-#       creds.refresh(Request())
-#     else:
-#       flow = InstalledAppFlow.from_service_account_file(
-#           "/tmp/activityAccountant/creds/credentials.json", SCOPES
-#       )
-#       creds = flow.run_local_server(port=0)
-#     # Save the credentials for the next run
-#     with open("token.json", "w") as token:
-#       token.write(creds.to_json())
+    try:
+        service = build(
+            "drive",
+            "v3",
+            credentials=service_account.Credentials.from_service_account_file(
+                CREDSFILE
+            ),
+        )
 
-  try:
-    service = build("drive", "v3", credentials=service_account.Credentials.from_service_account_file(CREDSFILE))
+        downloadDirectory(
+            service, getFolderIdByName(service, "ActivityAccounting"), "/tmp"
+        )
+        uploadSpreadsheet(
+            service,
+            getFolderIdByName(service, "scoring"),
+            "./test/eventExports/eventsList.xlsx",
+        )
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f"An error occurred: {error}")
 
-    
-    downloadDirectory(service,getFolderIdByName(service,"ActivityAccounting"),'/tmp')
-    uploadSpreadsheet(service,getFolderIdByName(service,"scoring"),'./test/eventExports/eventsList.xlsx')
-  except HttpError as error:
-    # TODO(developer) - Handle errors from drive API.
-    print(f"An error occurred: {error}")
 
 def getFolderIdByName(service, name):
-       # Call the Drive v3 API
+    # Call the Drive v3 API
     folderList = (
         service.files()
-        .list(pageSize=10,
-              q="mimeType = 'application/vnd.google-apps.folder' and name = '" + name + "'", 
-              fields="nextPageToken, files(id, name)",
-              includeItemsFromAllDrives=True,
-              supportsAllDrives=True)
+        .list(
+            pageSize=10,
+            q="mimeType = 'application/vnd.google-apps.folder' and name = '"
+            + name
+            + "'",
+            fields="nextPageToken, files(id, name)",
+            includeItemsFromAllDrives=True,
+            supportsAllDrives=True,
+        )
         .execute()
     )
     items = folderList.get("files", [])
     if not items:
-      print("Shared folder '" + name + "' not found.")
-      return
-    if (items.__len__() != 1):
-      print("There must be exactly one '" + name + "' folders shared with this user. Check sharing and try again.")
-      return
-    return items[0]['id']
+        print("Shared folder '" + name + "' not found.")
+        return
+    if items.__len__() != 1:
+        print(
+            "There must be exactly one '"
+            + name
+            + "' folders shared with this user. Check sharing and try again."
+        )
+        return
+    return items[0]["id"]
 
-def uploadSpreadsheet(service,parentFolderId,localPath):
-  try:
-    file_metadata = {"name": os.path.split(localPath)[-1], "parents": [parentFolderId]}
-    abspath =  os.path.abspath(localPath)
-    media = MediaFileUpload(
-       abspath, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", resumable=True
-    )
-    # pylint: disable=maybe-no-member
-    file = (
-        service.files()
-        .create(body=file_metadata, media_body=media, fields="id",supportsAllDrives=True)
-        .execute()
-    )
-    print(f'File ID: "{file.get("id")}".')
-    return file.get("id")
 
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return None
+def uploadSpreadsheet(service, parentFolderId, localPath):
+    try:
+        file_metadata = {
+            "name": os.path.split(localPath)[-1],
+            "parents": [parentFolderId],
+        }
+        abspath = os.path.abspath(localPath)
+        media = MediaFileUpload(
+            abspath,
+            mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            resumable=True,
+        )
+        # pylint: disable=maybe-no-member
+        file = (
+            service.files()
+            .create(
+                body=file_metadata,
+                media_body=media,
+                fields="id",
+                supportsAllDrives=True,
+            )
+            .execute()
+        )
+        print(f'File ID: "{file.get("id")}".')
+        return file.get("id")
 
-   
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return None
+
 
 if __name__ == "__main__":
-  main()
+    main()
