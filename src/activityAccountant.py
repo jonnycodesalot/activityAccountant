9ef13f3b4e98d6a490b4c6c49b460fa0315a25d4
Jonathan Hearn <jonnycodesalot@gmail.com>
2025-12-15 17:12:42 -0500

    Update latest event end date to be the end of league

--- src/activityAccountant.py
+++ src/activityAccountant.py
@@ -17,7 +17,7 @@ OLDEST_REGISTRANT_ALLOWED = dt.datetime(
     year=2023, month=9, day=1, hour=9, minute=0, second=0
 )
 # Overwritten by "now" in code if earlier than now.
-LATEST_EVENT_END_DATE = pd.to_datetime("2025/03/17")
+LATEST_EVENT_END_DATE = pd.to_datetime("2025/12/19")
 
 
 class Event:
